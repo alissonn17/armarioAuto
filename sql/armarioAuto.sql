9e20c44c979004fa431fc62fb9c14293dd31bbf2
Alisson <llielzinh@gmail.com>
2025-03-06 17:26:55 -0300

    :card_file_box: raw: sam´s entity

--- sql/armarioAuto.sql
+++ sql/armarioAuto.sql
@@ -8,4 +8,16 @@ nome varchar(50),
 email varchar(50),
 senha varchar(20) not null,
 saldo_cashback decimal(10, 2)
-)
+);
+
+CREATE TABLE produto( -- tabela produtos criada, necessario a tabela intermediaria com pedido para funcionamento completo
+	produto_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    unidade INT,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade INT,
+    categoria VARCHAR(20),
+    data_validade date,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
