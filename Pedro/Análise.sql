1327e75dc3e3fabbd9f67423b05cce9123d36ca9
Pedro de Sousa Freitas <pedrosousafreitas123@gmail.com>
2025-03-12 00:06:45 -0300

    3A Consulta

--- Pedro/Análise.sql
+++ Pedro/Análise.sql
@@ -5,3 +5,7 @@ inner join mercado on mercado.mercado_id = pedidos.mercado_id;
 select historico_consumo.quantidade_gramas, produto.nome
 from historico_consumo
 inner join produto on produto.produto_id = historico_consumo.produto_id;
+
+select metasNutricionias.meta_calorias, metasNutricionias.meta_gorduras, usuario.nome
+from metasNutricionias
+inner join usuario on usuario.usuario_id = metasNutricionias.usuario_id; 
